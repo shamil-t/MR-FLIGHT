99c623800882b4f0a670bcaaf8f24b31c4ab0eee
SHAMIL T <67453312+shamil-t@users.noreply.github.com>
2025-05-23 17:30:37 +0530

    Update ApiKey.java
    vulnerable api key removed

--- app/src/main/java/com/shamil/mr_flight/Videos/ApiKey.java
+++ app/src/main/java/com/shamil/mr_flight/Videos/ApiKey.java
@@ -1,5 +1,5 @@
 package com.shamil.mr_flight.Videos;
 
 public class ApiKey {
-    public static final String YOUTUBE_API_KEY = "AIzaSyCFYUh7fGvts5VYFiZiLqqIjmBC9hPStZM";
+    public static final String YOUTUBE_API_KEY = "API - KEY";
 }
